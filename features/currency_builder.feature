062f87e3aead3111e90381b79a5a9d1316ebf2dd
Michal Strugala <michal.jan.strugala@gmail.com>
2018-05-19 21:19:39 +0200

    Acceptance tests: added 'behave' module + 1 test scenario

---
+++ features/currency_builder.feature
@@ -0,0 +1,11 @@
+# -- FILE: features/currency_builder.feature
+
+Feature: Creating new currency
+  This feature allows us to create an instance
+  of new currency with all the exchange rates and name
+  chosen by the end-user.
+
+  Scenario: Duplication of exchange rate
+    Given empty builder
+    When user adds new exchange rate twice
+    Then previous exchange should get updated
---
+++ features/steps/currency_builder_steps.py
@@ -0,0 +1,22 @@
+# -- FILE: features/steps/currency_builder_steps.py
+
+from behave import given, when, then
+
+from models.currency_builder import CurrencyBuilder
+
+
+@given('empty builder')
+def step_impl(context):
+    context.builder = CurrencyBuilder()
+
+
+@when('user adds new exchange rate twice')
+def step_impl(context):
+    context.builder.add_currency('USD', 3.32)
+    context.builder.add_currency('USD', 2.56)
+
+
+@then('previous exchange should get updated')
+def step_impl(context):
+    currency = context.builder.build()
+    assert currency.get_exchange_vector()[-1][1] == 2.56
