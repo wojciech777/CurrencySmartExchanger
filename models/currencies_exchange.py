39f41e42491344ccdf10e1ce60d123a2fcade199
Piotr Glod <p.w.glod@gmail.com>
2018-06-14 09:42:05 +0200

    CurrenciesExchange model refactor, new tests added

--- models/currencies_exchange.py
+++ models/currencies_exchange.py
@@ -1,56 +1,65 @@
 class CurrenciesExchange:
 
     def __init__(self):
-        self.currencies = []
-        self.currency_dictionary = {}
+        self._currs = []
+        self._curr_dict = {}
+        self._unkn_currs = []
 
-    def add_currency(self, new_currency, default_exchange_currency):
-        for currency in self.currencies:
-            if not self.currency_dictionary[default_exchange_currency, currency]:
+    def add_currency(self, new_curr, def_exchange_curr):
+        for curr in self._currs:
+            if not self._curr_dict[def_exchange_curr, curr]:
                 raise ValueError("This default_exchange_currency can't be used, chose another")
+        self._currs.append(new_curr.get_name())
+        self._curr_dict[new_curr.get_name(), new_curr.get_name()] = 1
+        self._unkn_currs = []
+        self._add_new_currency(new_curr)
+        self._update_old_currencies(new_curr, def_exchange_curr)
+        for unkn_curr in self._unkn_currs:
+            self._currs.append(unkn_curr)
+        self._update_unknown_currencies(new_curr)
+        self._unkn_currs = []
 
-        self.currencies.append(new_currency.get_name())
-        self.currency_dictionary[new_currency.get_name(), new_currency.get_name()] = 1
-        unknown_currencies = []
+    def add_currencies(self, currs, def_exchange_curr):
+        for curr in currs:
+            self.add_currency(curr, def_exchange_curr)
 
-        for exchange_rate in new_currency.get_all_related_currencies_as_list():
-            if self.currencies.count(exchange_rate.get_name()) == 0:
-                unknown_currencies.append(exchange_rate.get_name())
-
-            self.currency_dictionary[
-                new_currency.get_name(), exchange_rate.get_name()] = exchange_rate.get_value()
-            self.currency_dictionary[
-                exchange_rate.get_name(), new_currency.get_name()] = 1 / exchange_rate.get_value()
-
-        for currency in self.currencies:
-            if not self.currency_dictionary[new_currency.get_name(), currency]:
-                self.currency_dictionary[new_currency.get_name(), currency] = self.currency_dictionary[
-                                                                                  new_currency.get_name(), default_exchange_currency] * \
-                                                                              self.currency_dictionary[
-                                                                                  default_exchange_currency, currency]
-                self.currency_dictionary[currency, new_currency.get_name()] = 1 / self.currency_dictionary[
-                    new_currency.get_name(), default_exchange_currency] * self.currency_dictionary[
-                                                                                  default_exchange_currency, currency]
-        for unknown_currency in unknown_currencies:
-            for currency in self.currencies:
-                self.currency_dictionary[unknown_currency, unknown_currency] = 1
-                if not self.currency_dictionary[unknown_currency, currency]:
-                    self.currency_dictionary[new_currency.get_name(), currency] = self.currency_dictionary[
-                                                                                      unknown_currency, new_currency.get_name()] * \
-                                                                                  self.currency_dictionary[
-                                                                                      new_currency.get_name(), currency]
-                    self.currency_dictionary[currency, new_currency.get_name()] = 1 / self.currency_dictionary[
-                        unknown_currency, new_currency.get_name()] * self.currency_dictionary[
-                                                                                      new_currency.get_name(), currency]
-        for unknown_currency in unknown_currencies:
-            self.currencies.append(unknown_currency)
-
-    def add_currencies(self, currencies, default_exchange_currency):
-        for currency in currencies:
-            self.add_currency(currency, default_exchange_currency)
-
-    def get_exchange_rate(self, source_currency, destination_currency):
-        return self.currency_dictionary[source_currency, destination_currency]
+    def get_exchange_rate(self, src_curr, dest_curr):
+        return self._curr_dict[src_curr, dest_curr]
 
     def get_currency_exchange_dictionary(self):
-        return self.currency_dictionary
+        return self._curr_dict
+
+    def get_currencies_names(self):
+        return self._currs
+
+    def _add_new_currency(self, new_curr):
+        for exchange_rate in new_curr.get_all_related_currencies_as_list():
+            if self._currs.count(exchange_rate.get_name()) == 0:
+                self._unkn_currs.append(exchange_rate.get_name())
+
+            self._curr_dict[new_curr.get_name(), exchange_rate.get_name()] = exchange_rate.get_value()
+            self._curr_dict[exchange_rate.get_name(), new_curr.get_name()] = 1 / exchange_rate.get_value()
+
+    def _update_old_currencies(self, new_curr, def_exchange_curr):
+        for curr in self._currs:
+            if not((new_curr.get_name(), curr) in self._curr_dict):
+                self._curr_dict[new_curr.get_name(), curr] = self._curr_dict[
+                                                                new_curr.get_name(), def_exchange_curr] * \
+                                                             self._curr_dict[
+                                                                def_exchange_curr, curr]
+                self._curr_dict[curr, new_curr.get_name()] = 1 / self._curr_dict[
+                    new_curr.get_name(), def_exchange_curr] * self._curr_dict[
+                                                                def_exchange_curr, curr]
+
+    def _update_unknown_currencies(self, new_curr):
+        for unkn_curr in self._unkn_currs:
+            for curr in self._currs:
+                self._curr_dict[unkn_curr, unkn_curr] = 1
+                if not((unkn_curr, curr) in self._curr_dict):
+                    self._curr_dict[unkn_curr, curr] = self._curr_dict[
+                                                                    unkn_curr, new_curr.get_name()] * \
+                                                          self._curr_dict[
+                                                                    new_curr.get_name(), curr]
+                    self._curr_dict[curr, unkn_curr] = 1 / self._curr_dict[
+                        unkn_curr, new_curr.get_name()] * self._curr_dict[
+                                                                    new_curr.get_name(), curr]
--- tests/unit_tests/models/test_currency_exchange.py
+++ tests/unit_tests/models/test_currency_exchange.py
@@ -10,9 +10,38 @@ class TestCurrencyExchangeClassMethods(unittest.TestCase):
     def test_add_currency(self):
         exchange = CurrenciesExchange()
         builder = CurrencyBuilder()
-        currency = builder.set_name('USD').add_currency('EUR', 1.45).build()
+        currency = builder.set_name('USD').add_currency('EUR', 1.45).add_currency('JEN', 2).build()
         exchange.add_currency(currency, 'USD')
-        print(exchange.get_exchange_rate('USD', 'EUR'))
+        self.assertEqual(exchange.get_exchange_rate('USD', 'EUR'), 1.45)
+        self.assertEqual(exchange.get_exchange_rate('USD', 'JEN'), 2)
+        self.assertEqual(exchange.get_exchange_rate('JEN', 'EUR'), 2.9)
+
+    def test_add_currencies(self):
+        exchange = CurrenciesExchange()
+        builder = CurrencyBuilder()
+        builder.set_name('USD')
+        builder.add_currency('EUR', 1.45)
+        currency = builder.build()
+        exchange.add_currency(currency, 'USD')
+
+        builder2 = CurrencyBuilder()
+        builder2.set_name('JEN')
+        builder2.add_currency('USD', 2).add_currency('PLN', 3)
+        currency2 = builder2.build()
+        exchange.add_currency(currency2, 'USD')
+
+        self.assertEqual(exchange.get_exchange_rate('JEN', 'EUR'), 2.9)
+
+    def test_get_currencies(self):
+        exchange = CurrenciesExchange()
+
+        builder = CurrencyBuilder()
+        builder.set_name('USD')
+        builder.add_currency('EUR', 1.45)
+        currency = builder.build()
+        exchange.add_currency(currency, 'USD')
+
+        self.assertEqual(exchange.get_currencies_names(), ['USD', 'EUR'])
 
     def test_adding_csv_currencies(self):
         exchange = CurrenciesExchange()
