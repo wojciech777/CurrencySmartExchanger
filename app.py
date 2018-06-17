c53cf30cbdf0ca5fc9f54390186a4c262d56b5b0
Michal Strugala <michal.jan.strugala@gmail.com>
2018-06-17 17:37:08 +0200

    Typos etc

--- app.py
+++ app.py
@@ -15,14 +15,14 @@ def __printMenu():
     print()
     print("[1] - Import currencies from CSV file")
     print("[2] - Define new currency with exchange rates")
-    print("[3] - Get all currencies exchange rates in a form of table")
+    print("[3] - Get all currencies exchange rates in the form of a table")
     print("[4] - Check exchange rate between two currencies")
     print("[5] - Exit")
 
 
 def __filenameRequest():
-    filename = input("please provide name of csv file in app_sample_data: ")
-    delimiter = input("please provide delimiter")
+    filename = input("Please provide name of csv file in app_sample_data: ")
+    delimiter = input("Please provide delimiter: ")
     return filename, delimiter
 
 
@@ -42,7 +42,7 @@ def __print_currency_exchange(currency, currenciesNames):
 def __print_currencies_exchange():
     currenciesNames = currenciesExchange.get_currencies_names()
     if len(currenciesNames) == 0:
-        print("Actually no currency has been provided")
+        print("Actually, no currency has been provided")
         return
     for currency in currenciesNames:
         __print_currency_exchange(currency, currenciesNames)
@@ -54,57 +54,57 @@ def __provide_any_key():
 
 
 def __let_user_define_new_currency():
-    currencyName = input("provide name for your currency")
+    currencyName = input("Provide name for your currency")
     while True:
-        mainExchangeCurrency = input("provide currency name for evaluating not provided exchange rates")
+        mainExchangeCurrency = input("Provide currency name for evaluating not provided exchange rates: ")
         if mainExchangeCurrency in currenciesExchange.get_currencies_names():
             break
-        print("you've provided unknown currency name")
+        print("You've provided unknown currency name")
 
     currencyBuilder = CurrencyBuilder()
     currencyBuilder.set_name(currencyName)
-    mainExchangeCurrencyExchangeRate = float(input("please provide exchange rate for main exchange currency"))
+    mainExchangeCurrencyExchangeRate = float(input("Please provide exchange rate for main exchange currency: "))
     currencyBuilder.add_currency(mainExchangeCurrency, mainExchangeCurrencyExchangeRate)
 
     while True:
-        print("press x - if you want to add new exchange rate")
-        userDecision = input("press ENTER otherwise")
+        print("P x - if you want to add new exchange rate")
+        userDecision = input("Press ENTER otherwise")
         if userDecision.__eq__("x"):
             currencyExchangeName = __infinitely_ask_for_valid_currency_name()
-            currencyExchangeRate = float(input("provide exchange rate for given currency"))
+            currencyExchangeRate = float(input("Provide exchange rate for given currency: "))
             currencyBuilder.add_currency(currencyExchangeName, currencyExchangeRate)
         else:
             break
 
     newCurrency = currencyBuilder.build()
     currenciesExchange.add_currency(newCurrency, mainExchangeCurrency)
-    print("thank you for providing new currency")
+    print("New currency added")
     __print_currency_exchange(currencyName, currenciesExchange.get_currencies_names())
 
 
 def __infinitely_ask_for_valid_currency_name():
     while True:
-        currencyExchangeName = input("print name of currency ")
+        currencyExchangeName = input("Provider name of the currency: ")
         names = currenciesExchange.get_currencies_names()
         if currencyExchangeName in names:
             break
-        print("you provided unknown currency - try again")
+        print("You have provided unknown currency - try again")
     return currencyExchangeName
 
 
 def __let_user_check_exchange_rate_beetween_two_currencies():
-    print("provide source currency name")
+    print("Provide source currency name: ")
     sourceCurrencyName = __infinitely_ask_for_valid_currency_name()
 
-    print("provide destination currency name")
+    print("Provide destination currency name: ")
     while True:
         secondCurrencyName = __infinitely_ask_for_valid_currency_name()
         if secondCurrencyName.__eq__(sourceCurrencyName):
-            print(f'exchange rate from {sourceCurrencyName} to {secondCurrencyName} is 1.0')
+            print(f'Exchange rate from {sourceCurrencyName} to {secondCurrencyName} is 1.0')
         else:
             break
 
-    print("exchange rate from {0} to {1} is {2}".format(sourceCurrencyName, secondCurrencyName, str(
+    print("Exchange rate from {0} to {1} is {2}".format(sourceCurrencyName, secondCurrencyName, str(
         currenciesExchange.get_exchange_rate(sourceCurrencyName, secondCurrencyName))))
 
 
@@ -117,7 +117,7 @@ def __infinitely_ask_for_valid_csv_filename_and_delimiter():
         if os.path.exists(test_path):
             break
         else:
-            print("there is no file " + filename + " in folder app_sample_data - try again?")
+            print("There is no file " + filename + " in folder app_sample_data - try again?")
 
     return test_path, delimiter
 
@@ -125,7 +125,7 @@ def __infinitely_ask_for_valid_csv_filename_and_delimiter():
 def __infinitely_ask_for_action_from_menu():
     while True:
         try:
-            userInput = int(input("provide your choice"))
+            userInput = int(input("Your choice: "))
         except ValueError:
             print("Not an integer! Try again.")
             __provide_any_key()
@@ -142,7 +142,7 @@ while True:
     userAction = __infinitely_ask_for_action_from_menu()
 
     if userAction == 5:
-        print("you've provided " + str(userAction) + " -  Good Bye")
+        print("You've provided " + str(userAction) + " -  Good Bye")
         break
 
     if userAction == 1:
@@ -163,6 +163,6 @@ while True:
         __let_user_check_exchange_rate_beetween_two_currencies()
 
     else:
-        print("you've provided unknown command number - try again")
+        print("You've provided unknown command number - try again")
 
     __provide_any_key()
